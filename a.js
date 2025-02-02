272ec3eced0c2fa11046dad645e05f34155cadd7
Shakil Raihan <137646085+Shakil10102@users.noreply.github.com>
2025-02-02 22:11:11 +0600

    Add files via upload

---
+++ a.js
@@ -0,0 +1,7 @@
+document.getElementById("yes").addEventListener("click", function () {
+    document.getElementById("response").innerHTML = "Love you too ❤️";
+});
+
+document.getElementById("no").addEventListener("click", function () {
+    alert("Think again... 💔");
+});
